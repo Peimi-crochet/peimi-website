6b49e93c3f3dd3ddb045dbdebd018d9a6c07e57c
Peimi-crochet <ririainipeimi@gmail.com>
2026-08-25 17:05:41 +0800

    Add sitewide analytics Clarity and IndexNow

---
+++ site-analytics.js
@@ -0,0 +1,17 @@
+(function(){
+  var gaId='G-H0CXPFT4TM';
+  var ga=document.createElement('script');
+  ga.async=true;
+  ga.src='https://www.googletagmanager.com/gtag/js?id='+gaId;
+  document.head.appendChild(ga);
+  window.dataLayer=window.dataLayer||[];
+  window.gtag=window.gtag||function(){window.dataLayer.push(arguments);};
+  window.gtag('js',new Date());
+  window.gtag('config',gaId);
+
+  (function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments);};
+    t=l.createElement(r);t.async=1;t.src='https://www.clarity.ms/tag/'+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window,document,'clarity','script','y5cvknahr7');
+})();
